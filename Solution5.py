e68de95805faf3daee4e838cc224736db5b8000d
Dhrumi <dhrumiprajapati115@gmail.com>
2023-09-21 14:57:01 +0530

    Solution5

---
+++ Solution5.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number a: "))
+b = int(input("Enter second number b: "))
+
+#swapping
+a,b = b,a
+
+print("Numbers after swap: ")
+print("a = " +str(a))
+print("b = " +str(b))
